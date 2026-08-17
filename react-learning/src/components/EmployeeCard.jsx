0a3d375b74c2eae8d4f31e8cbcc35c8bd76a64e7
Luv Risris <denluvris@gmail.com>
2026-08-17 09:58:47 +0700

    ใช้ useState กับ boolean

--- react-learning/src/components/EmployeeCard.jsx
+++ react-learning/src/components/EmployeeCard.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react"
+
 export default function EmployeeCard({
     name,
     department,
     position,
     isActive,
 }) {
+
+    const [isEmployeeActive, setIsEmployeeActive] = useState(isActive);
+    function handleToggleStatus(){
+        setIsEmployeeActive((currentStatus) => !currentStatus);
+    }
+
     return (
         <article className="employee-card">
             <h2>ข้อมูลพนักงาน</h2>
             <p>ชื่อ: {name}</p>
             <p>แผนก: {department}</p>
             <p>ตำแหน่ง: {position}</p>
-            <p>สถานะ: {isActive ? "กำลังฝึกงาน" : "ฝึกงานเสร็จสิ้น"}</p>
+            <p>สถานะ: {isEmployeeActive ? "กำลังทำงาน" : "ไม่ได้ทำงาน"}</p>
+            <button onClick={handleToggleStatus}>
+                {isEmployeeActive ? "ปิดสถานะพนักงาน":"เปิดสถานะพนักงาน"}
+            </button>
         </article>
     )
 }
